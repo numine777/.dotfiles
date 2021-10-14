4c716009c62b26ba598be5b101d06e1bacdf6da4
Scott Walls <scottwalls2008@gmail.com>
2021-10-14 09:22:42 -0500

    Update lvim config

--- nvim/.config/nvim/lua/lv-nvimtree/init.lua
+++ nvim/.config/nvim/lua/lv-nvimtree/init.lua
@@ -1,74 +1,141 @@
 local M = {}
-local nvim_tree_config = require("nvim-tree.config")
---
-M.config = function()
+-- local Log = require "dvim.core.log"
+
+function M.config()
+  dvim.builtin.nvimtree = {
+    active = true,
+    on_config_done = nil,
+    setup = {
+      open_on_setup = false,
+      auto_close = true,
+      open_on_tab = false,
+      update_focused_file = {
+        enable = true,
+      },
+      diagnostics = {
+        enable = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+      },
+      view = {
+        width = 30,
+        side = "left",
+        auto_resize = false,
+        mappings = {
+          custom_only = false,
+        },
+      },
+    },
+    show_icons = {
+      git = 1,
+      folders = 1,
+      files = 1,
+      folder_arrows = 1,
+      tree_width = 30,
+    },
+    ignore = { ".git", "node_modules", ".cache" },
+    quit_on_open = 0,
+    hide_dotfiles = 1,
+    git_hl = 1,
+    root_folder_modifier = ":t",
+    allow_resize = 1,
+    auto_ignore_ft = { "startify", "dashboard" },
+    icons = {
+      default = "",
+      symlink = "",
+      git = {
+        unstaged = "",
+        staged = "S",
+        unmerged = "",
+        renamed = "➜",
+        deleted = "",
+        untracked = "U",
+        ignored = "◌",
+      },
+      folder = {
+        default = "",
+        open = "",
+        empty = "",
+        empty_open = "",
+        symlink = "",
+      },
+    },
+  }
+end
+
+function M.setup()
+  local status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+  if not status_ok then
+    -- Log:error "Failed to load nvim-tree.config"
+    return
+  end
   local g = vim.g
 
-  vim.o.termguicolors = true
+  for opt, val in pairs(dvim.builtin.nvimtree) do
+    g["nvim_tree_" .. opt] = val
+  end
 
-  g.nvim_tree_side = "left"
-  g.nvim_tree_width = 50
-  -- g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
-  g.nvim_tree_auto_open = 1
-  g.nvim_tree_auto_close = 0
-  g.nvim_tree_quit_on_open = 0
-  g.nvim_tree_follow = 1
-  g.nvim_tree_indent_markers = 1
-  g.nvim_tree_hide_dotfiles = 1
-  g.nvim_tree_git_hl = 1
-  g.nvim_tree_root_folder_modifier = ":t"
-  g.nvim_tree_tab_open = 0
-  g.nvim_tree_allow_resize = 1
-  g.nvim_tree_lsp_diagnostics = 1
-  g.nvim_tree_auto_ignore_ft = { "startify", "dashboard" }
-  g.nvim_tree_disable_netrw = 0
+  -- Implicitly update nvim-tree when project module is active
+  if dvim.builtin.project.active then
+    dvim.builtin.nvimtree.respect_buf_cwd = 1
+    dvim.builtin.nvimtree.setup.update_cwd = true
+    dvim.builtin.nvimtree.setup.disable_netrw = false
+    dvim.builtin.nvimtree.setup.hijack_netrw = false
+    vim.g.netrw_banner = false
+  end
 
-  g.nvim_tree_show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-  }
-
-  vim.g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {
-      unstaged = "",
-      staged = "S",
-      unmerged = "",
-      renamed = "➜",
-      deleted = "",
-      untracked = "U",
-      ignored = "◌",
-    },
-    folder = {
-      default = "",
-      open = "",
-      empty = "",
-      empty_open = "",
-      symlink = "",
-    },
-  }
   local tree_cb = nvim_tree_config.nvim_tree_callback
 
-  vim.g.nvim_tree_bindings = {
-    { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-    { key = "h", cb = tree_cb "close_node" },
-    { key = "v", cb = tree_cb "vsplit" },
-  }
+  if not dvim.builtin.nvimtree.setup.view.mappings.list then
+    dvim.builtin.nvimtree.setup.view.mappings.list = {
+      { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+      { key = "h", cb = tree_cb "close_node" },
+      { key = "v", cb = tree_cb "vsplit" },
+    }
+  end
+
+  dvim.builtin.which_key.mappings["e"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" }
+
+  local tree_view = require "nvim-tree.view"
+
+  -- Add nvim_tree open callback
+  local open = tree_view.open
+  tree_view.open = function()
+    M.on_open()
+    open()
+  end
+
+  vim.cmd "au WinClosed * lua require('lv-nvimtree').on_close()"
+
+  if dvim.builtin.nvimtree.on_config_done then
+    dvim.builtin.nvimtree.on_config_done(nvim_tree_config)
+  end
+  require("nvim-tree").setup(dvim.builtin.nvimtree.setup)
 end
 
-local view_status_ok, view = pcall(require, "nvim-tree.view")
-if not view_status_ok then
-  return
-end
-M.toggle_tree = function()
-  if view.win_open() then
-    require("nvim-tree").close()
-  else
-    require("nvim-tree").find_file(true)
+function M.on_open()
+  if package.loaded["bufferline.state"] and dvim.builtin.nvimtree.setup.view.side == "left" then
+    require("bufferline.state").set_offset(dvim.builtin.nvimtree.setup.view.width + 1, "")
   end
 end
---
+
+function M.on_close()
+  local buf = tonumber(vim.fn.expand "<abuf>")
+  local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+  if ft == "NvimTree" and package.loaded["bufferline.state"] then
+    require("bufferline.state").set_offset(0)
+  end
+end
+
+function M.change_tree_dir(dir)
+  local lib_status_ok, lib = pcall(require, "nvim-tree.lib")
+  if lib_status_ok then
+    lib.change_dir(dir)
+  end
+end
+
 return M
