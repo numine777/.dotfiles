4a19807207fe33bfcdb398d62f5a813af589aad4
Scott Walls <scottwalls2008@gmail.com>
2021-10-14 19:03:27 -0500

    Starting DoomVim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,20 +1,24 @@
-require("config"):init()
-require("plugins")
-require("settings")
-require("keybindings")
-require("lv-utils")
-require("lv-treesitter")
-require("lv-git")
-require("lv-flutter-tools")
-require("lv-lspinstall")
-require("lv-neoformat")
-require("lv-lualine")
-require("lv-harpoon")
-require("lv-comment")
-require("lv-gitsigns")
+if os.getenv "DOOMVIM_RUNTIME_DIR" then
+  local path_sep = vim.loop.os_uname().version:match "Windows" and "\\" or "/"
+  vim.opt.rtp:append(os.getenv "DOOMVIM_RUNTIME_DIR" .. path_sep .. "dvim")
+end
 
-require("lsp")
-require("lsp.languages.python")
-require("lsp.languages.typescript")
-require("lsp.languages.sumneko")
-require("lsp.languages.bash")
+require("dvim.bootstrap"):init()
+
+require("dvim.config"):load()
+
+local plugins = require "dvim.plugins"
+require("dvim.plugin-loader"):load { plugins, dvim.plugins }
+
+local Log = require "dvim.core.log"
+Log:debug "Starting DoomVim"
+
+vim.g.colors_name = dvim.colorscheme -- Colorscheme must get called after plugins are loaded or it will break new installs.
+vim.cmd("colorscheme " .. dvim.colorscheme)
+
+local commands = require "dvim.core.commands"
+commands.load(commands.defaults)
+
+require("dvim.keymappings").setup()
+
+require("dvim.lsp").setup()
